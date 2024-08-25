491d15ce40ef2fb58a8e2f2b036777d5e051bc08
Roy Ran <ranyanxia@gmail.com>
2024-08-25 16:24:33 +0800

    @RR @RR init repo

---
+++ src/main/java/io/yanxia/ddd_service/DddServiceApplication.java
@@ -0,0 +1,13 @@
+package io.yanxia.ddd_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddServiceApplication.class, args);
+	}
+
+}
