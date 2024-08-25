9649491ff055d80b5d38ee0eec60bb8185121b7e
Roy Ran <ranyanxia@gmail.com>
2024-08-25 16:24:33 +0800

    @ranyanxia @ranyanxia init repo

---
+++ src/test/java/io/yanxia/ddd_service/DddServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yanxia.ddd_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
